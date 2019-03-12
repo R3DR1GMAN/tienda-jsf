56d3f9e1a9976e62a3d2e02eec5d5b6f1a376301
Rigo <cloud.rigman@gmail.com>
2019-03-11 19:53:36 -0500

    Mi demo de git add y git commit

--- tienda-jsf/src/main/java/pe/edu/cibertec/web/view/bean/LoginBean.java
+++ tienda-jsf/src/main/java/pe/edu/cibertec/web/view/bean/LoginBean.java
@@ -18,7 +18,7 @@ public class LoginBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-    private String correoElectronico;
+    private String 
     private String contrasena;
     private String mensajeError;
 
